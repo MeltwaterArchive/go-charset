2ec966ff514da1378a5bc343588a87edf7eb1bcc
Ollie Parsley <ollie@ollieparsley.com>
2016-02-01 12:32:49 +0000

    Update references to itself

--- charset/iconv/iconv_test.go
+++ charset/iconv/iconv_test.go
@@ -1,9 +1,9 @@
 package iconv_test
 
 import (
-	_ "code.google.com/p/go-charset/charset/iconv"
 	"bytes"
-	"code.google.com/p/go-charset/charset"
+	"github.com/datasift/go-charset/charset"
+	_ "github.com/datasift/go-charset/charset/iconv"
 	"io"
 	"strings"
 	"testing"
